82498c925b943165ad0ef9209baca1268a890c7a
Uthra Krishnan <uakrishnan@gmail.com>
2016-04-25 12:00:05 -0700

    add users migration

---
+++ migrations/20160425115558_create_users.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', table=>{
+    table.increments(),
+    table.text('username').unique(),
+    table.text('password'),
+    table.text('profile_pic')
+    table.date('dob')
+    table.text('blurb')
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('users');
+};
