09c48367e2fe84570a5f018a93f364bd37677d60
Uthra Krishnan <uakrishnan@gmail.com>
2016-04-26 14:09:26 -0700

    fixed seed files

--- seeds/seed_hours.js
+++ seeds/seed_hours.js
@@ -2,16 +2,10 @@
 exports.seed = function(knex, Promise) {
   return Promise.join(
     // Deletes ALL existing entries
-    knex('hours').del(), 
+    //knex('hours').del(), 
 
     // Inserts seed entries
-    knex('hours').insert({venue_id: 1, dayOfWeek: 6, opening: '22:00:00', closing: '02:00:00'}),
-    knex('hours').insert({venue_id: 1, dayOfWeek: 7, opening: '22:00:00', closing: '02:00:00'}),
-    knex('hours').insert({venue_id: 2, dayOfWeek: 5, opening: '21:00:00', closing: '02:00:00'}),
-    knex('hours').insert({venue_id: 2, dayOfWeek: 6, opening: '21:00:00', closing: '02:00:00'}),
-    knex('hours').insert({venue_id: 2, dayOfWeek: 7, opening: '21:00:00', closing: '02:00:00'}),
-    knex('hours').insert({venue_id: 3, dayOfWeek: 6, opening: '22:00:00', closing: '03:00:00'}),
-    knex('hours').insert({venue_id: 3, dayOfWeek: 7, opening: '22:00:00', closing: '03:00:00'}),
+
   
   );
 };
