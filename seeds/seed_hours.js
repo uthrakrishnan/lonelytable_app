523233c0199aef7284495358f1b68026da34b842
Uthra Krishnan <uakrishnan@gmail.com>
2016-04-26 12:15:34 -0700

    Update seed files to handle integers

--- seeds/seed_hours.js
+++ seeds/seed_hours.js
@@ -5,13 +5,13 @@ exports.seed = function(knex, Promise) {
     knex('hours').del(), 
 
     // Inserts seed entries
-    knex('hours').insert({venue_id: 1, dayOfWeek: '6', opening: '10:00PM', closing: '2:00AM'}),
-    knex('hours').insert({venue_id: 1, dayOfWeek: '7', opening: '10:00PM', closing: '2:00AM'}),
-    knex('hours').insert({venue_id: 2, dayOfWeek: '5', opening: '09:00PM', closing: '2:00AM'}),
-    knex('hours').insert({venue_id: 2, dayOfWeek: '6', opening: '09:00PM', closing: '2:00AM'}),
-    knex('hours').insert({venue_id: 2, dayOfWeek: '7', opening: '09:00PM', closing: '2:00AM'}),
-    knex('hours').insert({venue_id: 3, dayOfWeek: '6', opening: '10:00PM', closing: '3:00AM'}),
-    knex('hours').insert({venue_id: 3, dayOfWeek: '7', opening: '10:00PM', closing: '3:00AM'}),
+    knex('hours').insert({venue_id: 1, dayOfWeek: 6, opening: '22:00:00', closing: '02:00:00'}),
+    knex('hours').insert({venue_id: 1, dayOfWeek: 7, opening: '22:00:00', closing: '02:00:00'}),
+    knex('hours').insert({venue_id: 2, dayOfWeek: 5, opening: '21:00:00', closing: '02:00:00'}),
+    knex('hours').insert({venue_id: 2, dayOfWeek: 6, opening: '21:00:00', closing: '02:00:00'}),
+    knex('hours').insert({venue_id: 2, dayOfWeek: 7, opening: '21:00:00', closing: '02:00:00'}),
+    knex('hours').insert({venue_id: 3, dayOfWeek: 6, opening: '22:00:00', closing: '03:00:00'}),
+    knex('hours').insert({venue_id: 3, dayOfWeek: 7, opening: '22:00:00', closing: '03:00:00'}),
   
   );
 };
