3f67477182f1e53f925f90919589c8c9516e1ec1
Uthra Krishnan <uakrishnan@gmail.com>
2016-04-28 11:21:48 -0700

    fixed facebook callback

--- routes/auth.js
+++ routes/auth.js
@@ -10,7 +10,7 @@ const FacebookStrategy = require('passport-facebook').Strategy;
 passport.use(new FacebookStrategy({
 	clientID: process.env.FACEBOOK_KEY,
 	clientSecret: process.env.FACEBOOK_SECRET,
-	callbackURL: "http://localhost:3000/auth/facebook/callback",
+	callbackURL: "https://lonelytable-app.herokuapp.com/",
   	scope: ['email', 'public_profile']
   }, (accessToken, refreshToken, profile, done)=>{
   	knex('users').where('fb_id', profile.id).first().then(user=>{
