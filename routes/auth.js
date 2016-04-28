f1b7e1f484b87694af16e04a970b5e72f493a17d
Uthra Krishnan <uakrishnan@gmail.com>
2016-04-28 13:11:02 -0700

    got Facebook login to work on heroku

--- routes/auth.js
+++ routes/auth.js
@@ -5,12 +5,18 @@ const knex = require('../db/knex');
 const helpers = require('../helpers/authHelpers');
 const passport = require('passport');
 const FacebookStrategy = require('passport-facebook').Strategy;
-
+// eval(require("locus"))
+// if(app.get("env") === "development") {
+//   var callback = "http://localhost:3000/auth/facebook/callback"
+// } else {
+//   var callback = "https://lonelytable-app.herokuapp.com/auth/facebook/callback"
+// }
 
 passport.use(new FacebookStrategy({
 	clientID: process.env.FACEBOOK_KEY,
 	clientSecret: process.env.FACEBOOK_SECRET,
-	callbackURL: "https://lonelytable-app.herokuapp.com/auth/facebook/callback",
+  callbackURL: "http://localhost:3000/auth/facebook/callback",
+	// callbackURL: callback,
   	scope: ['email', 'public_profile']
   }, (accessToken, refreshToken, profile, done)=>{
   	knex('users').where('fb_id', profile.id).first().then(user=>{
@@ -42,6 +48,7 @@ passport.deserializeUser((id, done)=>{
 router.get('/facebook', passport.authenticate('facebook'));
 
 router.get('/facebook/callback', function(req,res,next){
+  // console.log("are we in here?")
   passport.authenticate('facebook',function(err, user) {
     
     if(err) return next(err)
