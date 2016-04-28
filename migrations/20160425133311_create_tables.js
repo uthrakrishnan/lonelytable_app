5588a8540c349c83cd0515a42090d1a927fb9a5a
Uthra Krishnan <uakrishnan@gmail.com>
2016-04-27 17:30:37 -0700

    fixed more migrations

--- migrations/20160425133311_create_tables.js
+++ migrations/20160425133311_create_tables.js
@@ -3,11 +3,11 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('tables', t=>{
     t.increments(),
     t.integer('venue_id').unsigned().index().references('venues.id').notNullable().onDelete('cascade'),
-    t.text('table_name'),
+    t.text('name'),
     t.integer('cost'),
     t.integer('maxCapacity'),
     t.text('description'),
-    t.text('table_status')
+    t.text('status')
   })
 };
 
--- migrations/20160425133809_create_reservations.js
+++ migrations/20160425133809_create_reservations.js
@@ -3,10 +3,10 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('reservations', t=>{
     t.increments(),
     t.integer('table_id').unsigned().index().references('tables.id').notNullable(),
-    t.date('date'),
+    t.timestamp('date'),
     t.integer('user_id').unsigned().index().references('users.id').notNullable(),
-    t.integer('pledge'),
-    t.text('partyType')
+    t.integer('pledge')
+    t.integer('seats')
   })
 };
 
