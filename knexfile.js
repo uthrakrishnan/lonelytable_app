9fe39a782da760eb0f65133eec18b989ad82ae79
Uthra Krishnan <uakrishnan@gmail.com>
2016-04-28 10:31:40 -0700

    updated database url

--- knexfile.js
+++ knexfile.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   production: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL,
+    connection: process.env.HEROKU_POSTGRESQL_ONYX_URL,
     pool: {
       min: 2,
       max: 10
